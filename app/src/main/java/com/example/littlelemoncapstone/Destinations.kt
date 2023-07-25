c5e99a23435b0bb92deb6a3be88c489a855af1e7
suletetes <sulea841@gmail.com>
2023-07-25 19:36:14 +0100

    new commit

--- app/src/main/java/com/example/littlelemoncapstone/Destinations.kt
+++ app/src/main/java/com/example/littlelemoncapstone/Destinations.kt
@@ -1,2 +1,18 @@
 package com.example.littlelemoncapstone
 
+
+interface Destinations {
+    val route: String
+}
+
+object Onboarding : Destinations {
+    override val route = "Onboarding"
+}
+
+object Home : Destinations {
+    override val route = "Home"
+}
+
+object Profile : Destinations {
+    override val route = "Profile"
+}
