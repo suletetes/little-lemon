dc3fce9e2dc91da0b39c3e9f07f0c5d63d20630a
suletetes <sulea841@gmail.com>
2023-07-25 21:49:47 +0100

    new commit

--- app/src/main/java/com/example/littlelemoncapstone/Profile.kt
+++ app/src/main/java/com/example/littlelemoncapstone/Profile.kt
@@ -1,86 +1,49 @@
 package com.example.littlelemoncapstone
 
 import android.content.SharedPreferences
-
 import androidx.compose.foundation.background
-
 import androidx.compose.foundation.border
-
 import androidx.compose.foundation.layout.Arrangement
-
 import androidx.compose.foundation.layout.Column
-
 import androidx.compose.foundation.layout.fillMaxWidth
-
 import androidx.compose.foundation.layout.height
-
 import androidx.compose.foundation.layout.padding
-
 import androidx.compose.foundation.layout.width
-
 import androidx.compose.foundation.shape.RoundedCornerShape
-
 import androidx.compose.material.Button
-
 import androidx.compose.material.MaterialTheme
-
 import androidx.compose.material.Text
-
 import androidx.compose.material.TextFieldDefaults
-
 import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.Alignment
-
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.tooling.preview.Preview
-
 import androidx.compose.ui.unit.dp
-
 import androidx.navigation.NavController
 
 
 // or just
 
 import androidx.compose.runtime.*
-
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.res.stringResource
-
 import androidx.compose.ui.text.TextStyle
-
 import androidx.compose.ui.text.font.FontWeight
-
 import androidx.compose.ui.unit.sp
 
 
 @Composable
-
 fun Profile(navController: NavController, sharedPreferences: SharedPreferences) {
-
     Column(
-
-
         verticalArrangement = Arrangement.Center,
-
         horizontalAlignment = Alignment.CenterHorizontally
-
     ) {
-
         LogoImage(
-
             modifier = Modifier
-
                 .width(200.dp)
-
                 .height(80.dp)
-
         )
-
         ProfileFormFields(navController = navController, sharedPreferences = sharedPreferences)
-
     }
 
 }
