7e4af8c895ebdf2c52d377d3fcd41a002bb3ae89
suletetes <sulea841@gmail.com>
2023-07-25 21:49:58 +0100

    new commit

--- app/src/main/java/com/example/littlelemoncapstone/Profile.kt
+++ app/src/main/java/com/example/littlelemoncapstone/Profile.kt
@@ -22,14 +22,14 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 
-// or just
 
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.res.stringResource
+//import androidx.compose.ui.text.TextStyle
+//import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.unit.sp
 
 
 @Composable
