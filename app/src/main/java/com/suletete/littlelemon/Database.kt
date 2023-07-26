1b61be17daca619783a8744efc942a3d59ba6bab
suletetes <sulea841@gmail.com>
2023-07-26 03:41:04 +0100

    commit

--- app/src/main/java/com/suletete/littlelemon/Database.kt
+++ app/src/main/java/com/suletete/littlelemon/Database.kt
@@ -1,9 +1,10 @@
 package com.suletete.littlelemon
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
-@Entity
+@Entity(tableName = "menu_items")
 data class MenuItemRoom(
     @PrimaryKey
     val id: Int,
@@ -15,18 +16,38 @@ data class MenuItemRoom(
 )
 
 @Dao
-interface MenuItemDao{
-    @Query("SELECT * FROM MenuItemRoom")
+interface MenuItemDao {
+    @Query("SELECT * FROM menu_items")
     fun getAll(): LiveData<List<MenuItemRoom>>
 
     @Insert
-    fun insertAll(vararg menuItems: MenuItemRoom)
+    suspend fun insertAll(vararg menuItems: MenuItemRoom)
 
-    @Query("SELECT (SELECT COUNT(*) FROM MenuItemRoom) == 0")
-    fun isEmpty(): Boolean
+    @Query("SELECT COUNT(*) FROM menu_items")
+    suspend fun getItemCount(): Int
+
+    @Query("DELETE FROM menu_items")
+    suspend fun deleteAll()
 }
 
 @Database(entities = [MenuItemRoom::class], version = 1)
-abstract class AppDatabase: RoomDatabase(){
+abstract class AppDatabase : RoomDatabase() {
     abstract fun menuItemDao(): MenuItemDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                val newInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                instance = newInstance
+                newInstance
+            }
+        }
+    }
 }
