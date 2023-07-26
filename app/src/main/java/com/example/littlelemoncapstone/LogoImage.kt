ce7542819680b2a050bfc8dd981b005900f30559
suletetes <sulea841@gmail.com>
2023-07-26 01:41:36 +0100

    new commit

--- app/src/main/java/com/example/littlelemoncapstone/LogoImage.kt
+++ app/src/main/java/com/example/littlelemoncapstone/LogoImage.kt
@@ -1,18 +1,10 @@
 package com.example.littlelemoncapstone
 
-import android.content.SharedPreferences
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 
 @Composable
 fun LogoImage(modifier: Modifier) {
